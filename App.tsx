fd31f5645ec8e14254e8330be2f1a05870e49164
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2022-03-03 20:22:53 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#282828",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pattern: {
+    position: "absolute",
   },
 });
