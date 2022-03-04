2986f235ee58449d85e6e9110bb22579db1e55da
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2022-03-04 10:13:51 -0300

    feat: :sparkles: configurando app.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "./src/styles/theme";
+
+import AppLoading from "expo-app-loading";
+
+import { useFonts } from "expo-font";
+import {
+  Epilogue_400Regular,
+  Epilogue_500Medium,
+  Epilogue_600SemiBold,
+  Epilogue_700Bold,
+  Epilogue_800ExtraBold,
+} from "@expo-google-fonts/epilogue";
+import {
+  Rajdhani_400Regular,
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+
+import { Router } from "./src/router";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Epilogue_400Regular,
+    Epilogue_500Medium,
+    Epilogue_600SemiBold,
+    Epilogue_700Bold,
+    Epilogue_800ExtraBold,
+    Rajdhani_400Regular,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Router />
+      <StatusBar barStyle="light-content" backgroundColor="#282828" />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#282828",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pattern: {
-    position: "absolute",
-  },
-});
